cae4312810807b8106a127a4950b927e3930c645
kennethkoch <kennykoch47@gmail.com>
2021-10-17 18:29:28 -0400

    updating server

--- server/app.js
+++ server/app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`server is listening http://localhost:${port}`)
+  console.log(`server is listening at http://localhost:${port}`)
 })
