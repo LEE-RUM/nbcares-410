4be8fedb15e063e1a55448e4dc0c739283b530d1
kennethkoch <kennykoch47@gmail.com>
2021-10-19 12:11:40 -0400

    adding capability to serve static files

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', function(req, res){
-    res.send('you made a GET request to /home')
+    res.send('you made a GET request to /home');
 })
 router.get('/home', function(req, res){
-    res.send('you made a GET request to /home')
+    res.sendFile(path.join(__dirname, '../../client/index.html'));
 })
 
 module.exports = router;
