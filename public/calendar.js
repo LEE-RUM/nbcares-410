3e769f5b89b4f8dc198f20a7e77847afe9eb47d4
Kenny Koch <kennykoch47@gmail.com>
2021-11-06 20:25:31 -0400

    refactoring calendar.js and main.js

---
+++ public/calendar.js
@@ -0,0 +1,52 @@
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './main.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, bootstrapPlugin ],
+      themeSystem: 'bootstrap',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      initialDate: '2021-10-25',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'Example Event',
+          start: '2021-10-26T12:00:00'
+        },
+        {
+          title: 'Example Event 5',
+          start: '2021-10-28T12:30:00'
+        },
+        {
+          title: 'Example Event 3',
+          start: '2021-10-27T12:00:00'
+        },
+        {
+          title: 'Example Event 2',
+          start: '2021-10-26T16:00:00'
+        },
+        {
+          title: 'Example Event 1',
+          start: '2021-10-26T14:00:00'
+        },
+      ]
+    });
+  
+    calendar.render();
+  });
