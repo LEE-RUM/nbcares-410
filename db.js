124067a3bae9c3a725893bdda724ec1e19844cdf
Kenny Koch <kennykoch47@gmail.com>
2021-11-07 13:24:46 -0500

    configuring db, adding test users

---
+++ db.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from '/.firebaseConfig.js'
+import { getDatabase, ref, set, get, child } from "firebase/database";
+
+const app = initializeApp(firebaseConfig);
+// import { getAnalytics } from "firebase/analytics";
+
+function writeUserData(userId, name, email) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+    });
+  }
+  
+  //adding a user with id 1 to the db
+  writeUserData(1,'test','test@test.com')
+  
+  
+  //reading user 1 info from db
+  let userId = 1;
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log('data should appear here:')
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
--- public/main.js
+++ public/main.js
@@ -1,18 +1,3 @@
 import './calendar.js'
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
-import { firebaseConfig } from '../.firebaseConfig.js'
-
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Get a reference to the database service
-const database = getDatabase(app);
-console.log(app)
-console.log(database)
-
-
-
+import '../db.js'
 
