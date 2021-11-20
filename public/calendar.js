03729ddb7c4913aec1ec65b35f1c3cdc7563c1b8
Kenny Koch <kennykoch47@gmail.com>
2021-11-20 18:47:03 -0500

    adding expanded event descriptions on mouse hover

--- public/calendar.js
+++ public/calendar.js
@@ -5,37 +5,33 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import bootstrapPlugin from '@fullcalendar/bootstrap';
 import 'bootstrap'
-
-// import {writeEventData} from '../db.js'
-import { eventsPromise } from '../db';
-
-// writeEventData(2,'test2')
-// // getEventData();
-// writeEventData(3, 'test3')
-// import eventsPromise from '../db.js'
-
-
-
-
+import { Tooltip } from 'bootstrap';
+// import './dist/main.js'
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './main.css';
+// import './dist/main.css'
+// import {writeEventData} from '../db.js'
+import { eventsPromise } from '../db';
 
 
-// const onEventClick = function(info) {
-//   console.log('event clicked', info.event.title)
-//   console.log(document.getElementById('calendarModal'))
-//   // $('#modalTitle').html('test');
-//   // $('#modalBody').html('test');
-//   // $('#calendarModal').modal();
-// }
-// let eventsArray = getAllEvents()
-// let eventsArray = eventsPromise.then((events)=>eventsArray=events)
+
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
+  // var calendarModal = document.getElementById('calendarModal')
+  // console.log(calendarModal)
+  
+  // const onEventClick = function(info) {
+  //   console.log('event clicked', info.event.title)
+  //   // console.log(calendarModal)
+  //   // $('#modalTitle').html('test');
+  //   // $('#modalBody').html('test');
+  //   // $('#calendarModal').modal();
+  // }
   
   var calendar = new Calendar(calendarEl, {
     eventDisplay: 'block',
+    // initialView: 'listWeek',
     plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, bootstrapPlugin ],
     themeSystem: 'bootstrap',
     headerToolbar: {
@@ -43,46 +39,45 @@ document.addEventListener('DOMContentLoaded', function() {
       center: 'title',
       right: 'dayGridMonth,listWeek'
     },  
-    // eventClick: function(info) {
-      //   alert(alert('Event: ' + info.event.title))
-      // },
+    eventClick: function(info) {
+
+      },
       navLinks: true, // can click day/week names to navigate views
       editable: true,
       dayMaxEvents: true,
       eventMaxStack: 1,
       displayEventTime: true,
-      events: [
-        {
-          title: 'Food Pantry',
-          start: '2021-11-10T11:30:00'
-        },  
-        {
-          title: 'Clothing Drive',
-          start: '2021-11-10T11:00:00'
-        },  
-        {
-          title: 'Hiring Event',
-          start: '2021-11-11T09:00:00'
-        }
-      ]  
-    });  
-    
-    // calendar.render();
-      // let eventsArray = [      
+      nowIndicator: true,
+      eventDidMount: function(info) {
+        var tooltip = new Tooltip(info.el, {
+          title: info.event.title,
+          placement: 'top',
+          trigger: 'hover',
+          container: 'body',
+          animation: true,
+          template: `<div class="tooltip" role="tooltip">
+          <div class="arrow"></div>
+          <div class="tooltip-inner"></div>
+          event location<br/> event description<br/>
+          event start time<br/> event end time</div>`
+        });
+      },
+      // events: [
       //   {
-      //       id: 'a',
-      //       title: 'Example Event',
-      //       start: '2021-11-15T12:00:00',
-      //       location:'test location',
-      //       description: 'test description'
-      //     }
-      //   ];
-        // calendar.addEventSource(eventsArray)
-        //   eventsPromise.then((res) => {
-          //     console.log('these are events', res)
-          //     // calendar.removeAllEvents()
-          //   })  
-          // eventsPromise.then((events)=>console.log(events))
+      //     title: 'Food Pantry',
+      //     start: '2021-11-10T11:30:00'
+      //   },  
+      //   {
+      //     title: 'Clothing Drive',
+      //     start: '2021-11-10T11:00:00'
+      //   },  
+      //   {
+      //     title: 'Hiring Event',
+      //     start: '2021-11-11T09:00:00'
+      //   }
+      // ]  
+    });  
+  
   let eventsArray = [];
   eventsPromise.then((events)=> {
     eventsArray = Object.values(events)
